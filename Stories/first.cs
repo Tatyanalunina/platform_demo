6e393ecf0c45d60be9778141a66f93b5e1027d26
Lunina Tatyana <Tatyanalunina12@yandex.ru>
2024-05-18 19:47:55 +0300

    add code

---
+++ Stories/first.cs
@@ -0,0 +1,12 @@
+using System;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        void main()
+        {
+            Console.Write("TEST");
+
+        }
+    }
+}
